518bf47e54c76612fe19554457eca229e6ef17ee
Hamid Raza <hamid.raza@greensill.com>
2020-09-28 07:56:40 +0530

    added the terraform cloud bacjkend

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "cloidilm-prod"
+
+    workspaces {
+      name = "cloudilm-networking"
+    }
+  }
+}
